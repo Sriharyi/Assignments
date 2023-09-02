2a370c58fa9bf800f3f9c0e1f41b9d75e9e0dc45
Sriharyi <96840856+Sriharyi@users.noreply.github.com>
2023-09-02 15:28:29 +0530

    fiboseries in BigInteger

--- JavaProgramming/Fibonacci.java
+++ JavaProgramming/Fibonacci.java
@@ -1,6 +1,8 @@
+import java.math.*;
+
 public class Fibonacci
 {
-	public static void fiboiterative(int n){
+	public static int[] fiboiterative(int n){
 		int n1 =0;
 		int n2 =1;
 		int fibo[] = new int[100000];
@@ -11,16 +13,52 @@ public class Fibonacci
 			fibo[i] = fibo[i-1]+fibo[i-2];
 		}
 		System.out.println(fibo[n]);
+		return fibo;
 	}
 	public static int fiborecursive(int n){
 	 if(n==0) return 0;
 	 if(n==1) return 1;
 	 return fiborecursive(n-1)+fiborecursive(n-2);
 	}
+	public static void fiboseries(int n){
+		int fibo[] = fiboiterative(n);
+		for(int i=0;i<n;i++)
+		{
+			System.out.print(fibo[i]+" ");
+		}
+	}
+	// public static void fiboseriesrecursive(long n){
+	// 	System.out.print("0 ");
+	// 	System.out.print("1 ");
+	// 		fiborecur(0,1,n-2);
+    //  		}
+    //  	public static void fiborecur(long n1,long n2,long n){
+    //  		if(n>=0)
+    //  		{
+    //  			long n3 = n1+n2;	
+    //  			System.out.print(n3+" ");
+    //  			fiborecur(n2,n3,n-1);
+	// 	}
+	// }
+	public static void fiboseriesrecursive(long n){
+		System.out.print("0 ");
+		System.out.print("1 ");
+			fiborecur(new BigInteger("0"),new BigInteger("1"),n-2);
+     		}
+     	public static void fiborecur(BigInteger n1,BigInteger n2,long n){
+     		if(n>=0)
+     		{
+     			BigInteger n3 = n1.add(n2);	
+     			System.out.print(n3+" ");
+     			fiborecur(n2,n3,n-1);
+		}
+	}
+     		
 	public static void main(String... args){
-
-		fiboiterative(Integer.parseInt(args[0]));
+		/*fiboiterative(Integer.parseInt(args[0]));
 		System.out.println(fiborecursive(Integer.parseInt(args[0])));
+		fiboseries(Integer.parseInt(args[0]));*/
+		fiboseriesrecursive(Long.parseLong(args[0]));
 		
 	}
 }
