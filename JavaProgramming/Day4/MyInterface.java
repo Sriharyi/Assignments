f639ddcf4192c75295366fc0a9f9046cc55ac21e
Sriharyi <96840856+Sriharyi@users.noreply.github.com>
2023-09-06 09:50:17 +0530

    Nested inteface in java

---
+++ JavaProgramming/Day4/MyInterface.java
@@ -0,0 +1,20 @@
+interface InterfaceA{
+    void methodinA();
+    interface InterfaceB{
+        void methodinB();
+    }
+}
+
+class MyInterface implements InterfaceA.InterfaceB{
+  
+     public void methodinB()
+    {
+        System.out.println("helloB");
+    }
+    public static void main(String... args)
+    {
+        InterfaceA.InterfaceB b = new MyInterface();
+        b.methodinB();
+
+    }
+}
