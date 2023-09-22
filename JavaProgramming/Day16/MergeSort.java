eb2042f1451a8996eb901e98e43cf26a73b8d148
Sriharyi <96840856+Sriharyi@users.noreply.github.com>
2023-09-22 19:21:01 +0530

    mergeSortOptimal

--- JavaProgramming/Day16/MergeSort.java
+++ JavaProgramming/Day16/MergeSort.java
@@ -1,12 +1,11 @@
 import java.util.*;
 
 public class MergeSort{
-
-    public static void mergeSort(int arr[])
-    {
-        mergeSort(arr,0,arr.length-1);
-    }
     
+    public static void mergeSort(int arr[])
+     {
+        mergeSort(arr, 0, arr.length-1);
+     }
     private static void mergeSort(int[] arr, int low, int high) {
         if(low < high){
             int mid = ( low + high ) / 2; //or low + ((high-low)/2)
@@ -16,10 +15,10 @@ public class MergeSort{
         }
     }
 
-    private static void merge(int[] arr, int low, int mid, int high) {
+    private static void merge(int[] arr, int low,  int mid, int high) {
         int n1 = mid - low +1;
         int n2 = high - mid;
-
+        
         int leftSubarr[]= new int[n1];
         int rightSubarr[] = new int[n2];
 
@@ -52,12 +51,47 @@ public class MergeSort{
         while(j<n2){
             arr[k++] = rightSubarr[j++];
         }
-
+        
     }
-
+    
+    
     public static void main(String[] args) {
         int arr[] = new int[]{56,345,23,26,277,34,33,78,26,734,98,33,13};
         mergeSort(arr);
+        // arr = mergeSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 }
+        
+            // public static int[] mergeSort(int arr[])
+            // {
+            //     if(arr.length==1)
+            //         return arr;
+            //     int mid = arr.length/2;
+            //     int left[] = mergeSort(Arrays.copyOfRange(arr, 0, mid));
+            //     int right[] = mergeSort(Arrays.copyOfRange(arr, mid, arr.length));
+            //     return merge(left,right);
+            // }
+        // private static int[] merge(int[] leftSubarr, int[] rightSubarr) {
+        //     int newArray[] = new int[leftSubarr.length + rightSubarr.length];
+    
+        //     int i=0,j=0,k=0;
+    
+        //      while(i<leftSubarr.length && j<rightSubarr.length)
+        //     {
+        //         if(leftSubarr[i]<=rightSubarr[j])
+        //         {
+            //             newArray[k++] = leftSubarr[i++];
+            //         }else
+            //         {
+        //             newArray[k++] = rightSubarr[j++];
+        //         }
+        //     }
+        //      while(i<leftSubarr.length) {
+        //         newArray[k++] = leftSubarr[i++];
+        //     }
+        //     while(j<rightSubarr.length){
+        //         newArray[k++] = rightSubarr[j++];
+        //     }
+        //     return newArray;
+        // }
